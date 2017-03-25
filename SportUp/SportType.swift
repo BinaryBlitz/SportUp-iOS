2ca1596773d559437aa0d5cd137141d24b80b4fb
blkshdw <aleksey.glebow@gmail.com>
2017-03-25 19:35:18 +0300

    Sport types screen: UI

---
+++ SportUp/SportType.swift
@@ -0,0 +1,27 @@
+//
+//  SportType.swift
+//  SportUp
+//
+//  Created by Алексей on 25.03.17.
+//  Copyright © 2017 binaryblitz. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class SportType: Mappable {
+  var id: Int = 0
+  var color: String = ""
+  var name: String = ""
+  var iconUrl: URL? = nil
+
+  func mapping(map: Map) {
+    id <- map["id"]
+    color <- map["color"]
+    name <- map["name"]
+    iconUrl <- (map["icon_url"], URLTransform())
+  }
+
+  required init(map: Map) { }
+  
+}
